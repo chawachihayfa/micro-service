58b1e6e2a0d8a989de24360bb5af71e49dff1111
chawachihayfa <chawachihayfa@gmail.com>
2024-07-24 19:46:54 +0200

    f2

--- clientMs/src/main/java/tn/esprit/clientms/repositoire/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.clientms.repositoire;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.clientms.entity.Client;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
